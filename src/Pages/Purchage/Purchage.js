97410543a4dc8a9645f310040bb77d1c671b9d7e
Eshratjahan17 <eshratjahan701@gmail.com>
2022-05-24 03:47:33 +0600

    adding parchase form

--- src/Pages/Purchage/Purchage.js
+++ src/Pages/Purchage/Purchage.js
@@ -1,53 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Purchage = () => {
    let {id}=useParams();
    console.log(id);
+   
    const [toolDetails,setToolDetails]=useState([]);
-   useEffect(()=>{
+   useEffect(() => {
      fetch(`http://localhost:5000/tools/${id}`)
-     .then(res=>res.json())
-     .then(data =>setToolDetails(data))
-   },[])
- 
+       .then((res) => res.json())
+       .then((data) => setToolDetails(data));
+   }, []);
+  
+   
+  
+   
   
   return (
     <div>
-      <div>
-        <div class="card w-96 bg-secondary shadow-xl transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300">
-          <figure>
-            <img src={toolDetails.picture} alt="Shoes" />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title text-white">{toolDetails.name}</h2>
-            <p className="text-white">{toolDetails.about}</p>
-            <p className="text-white font-semibold">
-              Price:$<span className="text-lg">{toolDetails.price}</span>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <div class="text-center lg:text-left">
+            <h1 class="text-5xl font-bold">{toolDetails.name}</h1>
+            <p class="py-6">
+              <p class="py-6">{toolDetails.about}</p>
+              Price: {toolDetails.Price}
             </p>
-            <div class="card-actions justify-between my-3">
-              <div class="badge badge-outline text-white p-3 font-semibold">
-                Minimum Order:
-                <span className="text-lg">{toolDetails.minimum}</span>
+            <p class="py-6">Available: {toolDetails.available}</p>
+            <p class="py-6">Minimum-Order: {toolDetails.minimum}</p>
+          </div>
+          <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div class="card-body">
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name here"
+                  class="input input-bordered"
+                />
               </div>
-              <div class="badge badge-outline p-3 text-white font-semibold">
-                Available:
-                <span className="text-lg">{toolDetails.available}</span>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  class="input input-bordered"
+                />
               </div>
-            </div>
-            <div className=" flex justify-center">
-              <Link
-                to="/purchage"
-                class="btn bg-primary text-white hover:bg-white hover:text-primary rounded-full mt-5"
-              >
-                +
-              </Link>
-              <Link
-                to="/purchage"
-                class="btn bg-primary text-white hover:bg-white hover:text-primary rounded-full mt-5"
-              >
-               -
-              </Link>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Address</span>
+                </label>
+                <textarea
+                  class="textarea textarea-bordered"
+                  placeholder="Your Address"
+                ></textarea>
+                <input type="radio" name="click" value="value" />
+                <label class="label">
+                  <a href="#" class="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-primary">Login</button>
+              </div>
             </div>
           </div>
         </div>
